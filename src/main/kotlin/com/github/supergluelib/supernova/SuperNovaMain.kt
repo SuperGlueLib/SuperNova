42a87f419afc7053b4ff353393683b75237a2e5a
MLGPenguin <baww123@gmail.com>
2025-01-18 09:45:21 +1000

    Load players on join

--- src/main/kotlin/com/github/supergluelib/supernova/SuperNovaMain.kt
+++ src/main/kotlin/com/github/supergluelib/supernova/SuperNovaMain.kt
@@ -1,8 +1,13 @@
 package com.github.supergluelib.supernova
 
+import com.github.supergluelib.foundation.extensions.registerListeners
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class SuperNovaMain: JavaPlugin() {
+class SuperNovaMain: JavaPlugin(), Listener {
 
     companion object {
         lateinit var instance: SuperNovaMain private set
@@ -11,7 +16,14 @@ class SuperNovaMain: JavaPlugin() {
     override fun onEnable() {
         instance = this
 
+        registerListeners(this)
+
         SuperNova // Load
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun loadPlayerOnJoin(event: PlayerJoinEvent) {
+        SuperNova.loadPlayer(event.player)
     }
 
 }
