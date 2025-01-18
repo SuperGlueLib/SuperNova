c685c48de27c25f1524de6cddccc1569ff1f5eda
MLGPenguin <baww123@gmail.com>
2025-01-18 13:53:36 +1000

    Create tables

--- src/main/kotlin/com/github/supergluelib/supernova/DBHandler.kt
+++ src/main/kotlin/com/github/supergluelib/supernova/DBHandler.kt
@@ -1,22 +1,36 @@
 package com.github.supergluelib.supernova
 
 import com.github.supergluelib.foundation.database.SQLiteDatabase
+import com.github.supergluelib.foundation.extensions.executeQuery
+import com.github.supergluelib.foundation.extensions.toUUID
 import com.github.supergluelib.supernova.SuperNova.PlayerKey
 import org.bukkit.NamespacedKey
-import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 import java.util.*
 
 internal class DBHandler(val plugin: SuperNovaMain) {
     private val db = SQLiteDatabase(File(plugin.dataFolder, "data.sqlite"))
 
-    // TODO: Loading on Join/Load, etc.
-    internal val pluginEntries: HashMap<NamespacedKey, String> = hashMapOf()
-    internal val playerEntries: HashMap<PlayerKey, String> = hashMapOf()
+    private val pluginEntries: HashMap<NamespacedKey, String> = hashMapOf()
+    private val playerEntries: HashMap<PlayerKey, String> = hashMapOf()
 
-    internal fun loadEntries(plugin: JavaPlugin): Unit = TODO() // LOAD ALL PLUGINS ON STARTUP.
-    internal fun loadEntries(uuid: UUID): Unit = TODO()
+    internal fun loadEntries(uuid: UUID) {
+        val rs = GET_BY_UUID_QUERY.executeQuery(uuid to UUID::class.java)
+        while (rs.next()) {
+            val key = PlayerKey(rs.getString("namespace"), rs.getString("uuid").toUUID()!!, rs.getString("key"))
+            playerEntries[key] = rs.getString("value")
+        }
+    }
 
+    internal fun unloadEntries(uuid: UUID) {
+        // TODO: If writes are replicated immediately, no need to save here -- double check
+        playerEntries.filter { it.key.uuid == uuid }.forEach { playerEntries.remove(it.key) }
+    }
+
+    init {
+        createTables()
+        // TODO: Load Plugins
+    }
 
     fun writePluginEntry(key: NamespacedKey, value: String) { // Can be new or existing, will overwrite.
         pluginEntries[key] = value
@@ -33,6 +47,53 @@ internal class DBHandler(val plugin: SuperNovaMain) {
 
     // EXPLAIN QUERY PLAN SELECT * FROM main_data WHERE id = 1;
     // INSERT OR IGNORE
-    // UNIQUE improves performance
+
+    private val GET_BY_UUID_QUERY = db.prepareStatement("""
+        SELECT (namespace_mapping.namespace, uuid_mapping.uuid, key, value) 
+        FROM player_data
+        JOIN namespace_mapping ON player_data.namespace = namespace_mapping.id
+        JOIN uuid_mapping ON player_data.uuid = uuid_mapping.id
+        WHERE uuid_mapping.uuid = ?;"
+    """.trimIndent())
+
+    private val INSERT_UUID_QUERY = db.prepareStatement("INSERT OR IGNORE INTO uuid_mapping (uuid) VALUES (?);")
+
+    private fun createTables() {
+        db.prepareStatement("""
+            CREATE TABLE IF NOT EXISTS namespace_mapping(
+              id INT PRIMARY KEY AUTOINCREMENT,
+              namespace TEXT NOT NULL UNIQUE
+            );
+        """.trimIndent()).execute()
+
+        db.prepareStatement("""
+            CREATE TABLE IF NOT EXISTS uuid_mapping(
+              id INT PRIMARY KEY AUTOINCREMENT,
+              uuid CHAR(36) NOT NULL UNIQUE
+            );
+        """.trimIndent()).execute()
+
+        db.prepareStatement("""
+          CREATE TABLE IF NOT EXISTS plugin_data(
+            namespace INT NOT NULL,
+            key TEXT NOT NULL,
+            value TEXT NOT NULL,
+            PRIMARY KEY (namespace, key),
+            FOREIGN KEY (namespace) REFERENCES namespace_mapping (id) 
+          );
+    """.trimIndent()).execute()
+
+        db.prepareStatement("""
+            CREATE TABLE IF NOT EXISTS player_data(
+              namespace INT NOT NULL,
+              uuid INT NOT NULL,
+              key TEXT NOT NULL,
+              value TEXT NOT NULL,
+              PRIMARY KEY (namespace, uuid, key),
+              FOREIGN KEY (namespace) REFERENCES namespace_mapping (id),
+              FOREIGN KEY (uuid) REFERENCES uuid_mapping (id)
+            )
+        """.trimIndent()).execute()
+    }
 
 }
